3ebd192e91081b82e2557ce8e95ad7851b6ab1cb
Aydan Lau <aydanlau@gmail.com>
2024-08-27 01:00:02 +0800

    Finished lab #1 (dateable and pages)

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,23 @@
-public class Dateable {
+import java.util.Scanner;
 
+public class Dateable {
     public static void main(String[] args) {
-        
+        // Take input
+        Scanner ageScanner = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+
+        // Repeat until integer input
+        int age = -1;
+        while (age < 0) {
+            try {
+                age = Integer.parseInt(ageScanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Enter an integer.");
+            }
+        }
+
+        // Calculate result
+        int dateAge = age / 2 + 7;
+        System.out.println(age + "-year olds should date somebody who is at least " + dateAge + " years old.");
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class Pages {
-
     public static void main(String[] args) {
+        // Take input
+        Scanner ageScanner = new Scanner(System.in);
+        System.out.print("Enter your age: ");
 
+        // Repeat until integer input
+        int age = -1;
+        while (age < 0) {
+            try {
+                age = Integer.parseInt(ageScanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Enter an integer.");
+            }
+        }
+
+        // Calculate result
+        int page = 100 - age;
+        System.out.println(age + "-year olds should read at least " + page + " pages before giving up on a book.");
     }
 }
