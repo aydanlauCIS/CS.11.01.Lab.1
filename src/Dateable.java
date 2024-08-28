7406027240b30347bd5c7b2391885f0c398f3f3b
Aydan Lau <aydanlau@gmail.com>
2024-08-28 09:48:08 +0800

    Finished lab #1 (dateable and pages)

--- src/Dateable.java
+++ src/Dateable.java
@@ -4,13 +4,18 @@ public class Dateable {
     public static void main(String[] args) {
         // Take input
         Scanner ageScanner = new Scanner(System.in);
-        System.out.print("Enter your age: ");
 
         // Repeat until integer input
-        int age = -1;
-        while (age < 0) {
+        int age;
+        while (true) {
+            System.out.print("Enter your age: ");
             try {
                 age = Integer.parseInt(ageScanner.nextLine());
+                if (age < 0) {
+                    System.out.println("Age has to be positive.");
+                } else {
+                    break;
+                }
             } catch (NumberFormatException e) {
                 System.out.println("Enter an integer.");
             }
